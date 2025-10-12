b617e6d0b82892b81521122d0e3fbe7f6c6bc27e
Devson Bwambale <devsonwebdev@gmail.com>
2025-10-12 20:27:43 +0300

    FlixMax prep completion

--- src/js/dataBase.js
+++ src/js/dataBase.js
@@ -47,7 +47,229 @@ function getNews() {
         console.log(response);
     })
 }
-//TRENDING MOVIES
-//TRENDING ANIME
+function movieNews() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://moviesverse1.p.rapidapi.com/movie-news',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
+        }
+    };
 
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+//TRENDING MOVIES
+function mostTrendingMvies() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://moviesverse1.p.rapidapi.com/new-horror',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+function horroMovies() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://moviesverse1.p.rapidapi.com/new-horror',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+function movieGenre() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://moviesverse1.p.rapidapi.com/movies-by-genre?genre=action',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+//TRENDING ANIME
+function popularAnime() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://anime-data-scraper-api.p.rapidapi.com/v1/anime/popular',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'anime-data-scraper-api.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+function topAnimeMovies() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://anime-data-scraper-api.p.rapidapi.com/v1/anime/top/movie',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'anime-data-scraper-api.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+}
+function topRankAnime() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://anime-data-scraper-api.p.rapidapi.com/v1/anime/top/rank',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'anime-data-scraper-api.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+function topTvAnime() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://anime-data-scraper-api.p.rapidapi.com/v1/anime/top/tv',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'anime-data-scraper-api.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+function topAnimeShows() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://moviesverse1.p.rapidapi.com/top-anime-shows',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'moviesverse1.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+// AI IMAGE GENERATOR
+function AiImageGenerator() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://ai-text-to-image-generator-flux-free-api.p.rapidapi.com/aaaaaaaaaaaaaaaaaiimagegenerator/fluximagegenerate/generateimage.php',
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'ai-text-to-image-generator-flux-free-api.p.rapidapi.com',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: { "prompt": "Hulk", "style_id": 4, "size": "1-1" }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+// TRENDING TRAILERS
+function getTrendingTrailers() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://api.kinocheck.com/trailers/trending",
+        method: 'GET'
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+// LATEST TRAILERS
+function getLatestTrailers() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://api.kinocheck.com/trailers/latest",
+        method: 'GET'
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
 // MOVIE QUOTES
+// 100
+function get100Quotes() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://movie-and-tv-shows-quotes.p.rapidapi.com/quotes',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'movie-and-tv-shows-quotes.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
+function randomQuotes() {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://movie-and-tv-shows-quotes.p.rapidapi.com/quotes/random/quote',
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+            'x-rapidapi-host': 'movie-and-tv-shows-quotes.p.rapidapi.com'
+        }
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,19 @@ import "../../node_modules/jquery/dist/jquery.min.js";
 $(document).ready(function () {
     function test() {
         const settings = {
-            async: true,
-            crossDomain: true,
-            url: 'https://movies-tv-shows-database.p.rapidapi.com/?year=2025&page=1',
-            method: 'GET',
-            headers: {
-                'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
-                'x-rapidapi-host': 'movies-tv-shows-database.p.rapidapi.com',
-                Type: 'get-shows-byyear'
-            }
-        };
+	async: true,
+	crossDomain: true,
+	url: 'https://anime-data-scraper-api.p.rapidapi.com/v1/anime/popular',
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e9bd911239msh07e14fa4c67bbf5p132d38jsn345fdb1b0020',
+		'x-rapidapi-host': 'anime-data-scraper-api.p.rapidapi.com'
+	}
+};
 
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        })
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
     }
     test()
 })
